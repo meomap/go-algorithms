01778fbc7e99ea44e6b20b14b381bbc9b868e74d
Hai Son <shnt1221@gmail.com>
2018-07-05 11:07:09 +0700

    longest common prefix divide and conquor

--- algorithms/math/longest-common-prefix/longest.go
+++ algorithms/math/longest-common-prefix/longest.go
@@ -31,7 +31,38 @@ LOOP:
 	return out.String(), nil
 }
 
-// DivideAndConquor implement strategy to merge to common longest prefix
+func mergeCommonPrefixes(left, right string) (string, error) {
+	lenL, lenR := len(left), len(right)
+	i := 0
+	out := bytes.NewBuffer(nil)
+	var err error
+	for i < lenL && i < lenR && left[i] == right[i] {
+		if err = out.WriteByte(left[i]); err != nil {
+			return "", err
+		}
+		i++
+	}
+	return out.String(), nil
+}
+
+func longestCommonPrefix(in []string) (string, error) {
+	lenIn := len(in)
+	if lenIn == 1 {
+		return in[0], nil
+	}
+	mid := lenIn / 2
+	lc, err := longestCommonPrefix(in[:mid])
+	if err != nil {
+		return "", err
+	}
+	rc, err := longestCommonPrefix(in[mid:])
+	if err != nil {
+		return "", err
+	}
+	return mergeCommonPrefixes(lc, rc)
+}
+
+// DivideAndConquor implements strategy to merge two halves common longest prefix
 func DivideAndConquor(in []string) (string, error) {
-	return "", nil
+	return longestCommonPrefix(in)
 }
--- algorithms/math/longest-common-prefix/longest_test.go
+++ algorithms/math/longest-common-prefix/longest_test.go
@@ -2,21 +2,42 @@ package longest
 
 import "testing"
 
-var (
-	testIn       = []string{"geeksforgeeks", "geeks", "geek", "geezer"}
-	testExpected = "gee"
-)
+type testData struct {
+	in       []string
+	expected string
+}
+
+var testCases = []testData{
+	{
+		in:       []string{"geeksforgeeks", "geeks", "geek", "geezer"},
+		expected: "gee",
+	},
+	{
+		in:       []string{"apple", "ape", "april"},
+		expected: "ap",
+	},
+}
 
 func TestBruteForce(t *testing.T) {
-	out, err := BruteForce(testIn)
-	if err != nil {
-		t.Fatalf("Unexpected error %q", err)
-	}
-	if testExpected != out {
-		t.Errorf("Expect %q but got %q", testExpected, out)
+	for _, c := range testCases {
+		out, err := BruteForce(c.in)
+		if err != nil {
+			t.Fatalf("Unexpected error %q", err)
+		}
+		if c.expected != out {
+			t.Errorf("Expect %q but got %q", c.expected, out)
+		}
 	}
 }
 
 func TestDivideAndConquor(t *testing.T) {
-
+	for _, c := range testCases {
+		out, err := DivideAndConquor(c.in)
+		if err != nil {
+			t.Fatalf("Unexpected error %q", err)
+		}
+		if c.expected != out {
+			t.Errorf("Expect %q but got %q", c.expected, out)
+		}
+	}
 }
